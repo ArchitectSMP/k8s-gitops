03481cc49904a90bc93247b3aa7f52d822966160
Chip Wolf <hello@chipwolf.uk>
2021-03-17 22:25:46 +0000

    feat: add mailu (fixes #128)

--- setup/bootstrap-vault.sh
+++ setup/bootstrap-vault.sh
@@ -179,6 +179,7 @@ loadSecretsToVault() {
   #################### 
   kvault "default/web/ghost-helm-values.txt"
   kvault "kube-system/oauth2-proxy/oauth2-proxy-helm-values.txt"
+  kvault "mailu/mailu-helm-values.txt"
   kvault "monitoring/grafana/grafana-helm-values.txt"
   kvault "monitoring/kube-prometheus-stack/kube-prometheus-stack-helm-values.txt"
   kvault "monitoring/minio/minio-helm-values.txt"
